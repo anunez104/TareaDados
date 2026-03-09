080426ca3ed7edd3de2a7292d306cbf94e3fd8d3
angie nunez <anunez104@unab.edu.co>
2026-03-08 23:13:00 -0500

    feat: imagenes2

--- app/src/main/java/com/angienunez/tareadados/StateDeditos.kt
+++ app/src/main/java/com/angienunez/tareadados/StateDeditos.kt
@@ -1,21 +1,60 @@
 package com.angienunez.tareadados
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.w3c.dom.Text
 
 @Composable
-fun StateDaditos (){
-var numero by remember { mutableStateOf(1) }
-val list = listOf(
-    R.drawable.dice_1,
-    R.drawable.dice_2,
-    R.drawable.dice_3,
-    R.drawable.dice_4,
-    R.drawable.dice_5,
-    R.drawable.dice_6
-)
-
+fun StateDaditos() {
+    var numero by remember { mutableStateOf(1) }
+    val list = listOf(
+        R.drawable.dice_1,
+        R.drawable.dice_2,
+        R.drawable.dice_3,
+        R.drawable.dice_4,
+        R.drawable.dice_5,
+        R.drawable.dice_6
+    )
+    Scaffold() { innerPadding ->
+        Text(
+            text = "Lanzar dado App",
+            fontSize = 22.sp,
+            modifier = Modifier
+                .background(Color(0xFFEBE1FF))
+                .padding(30.dp)
+                .fillMaxWidth()
+        )
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(list[numero - 1]),
+                contentDescription = null,
+                modifier = Modifier.size(200.dp)
+            )
+        }
+    }
 }
